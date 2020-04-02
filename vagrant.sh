98af82d73a63360ff691a8d1c07c93f63721f3d1
Juan Mancebo <juan.mancebo@privacyrequired.com>
2020-04-02 08:42:48 +0200

    Added vagrant default authorized key inside script

--- vagrant.sh
+++ vagrant.sh
@@ -24,9 +24,11 @@ echo "UseDNS no" >> /etc/ssh/sshd_config
 cd $VAGRANT_HOME
 mkdir -m 700 -p .ssh
 cd .ssh
-wget https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub -O authorized_keys
+cat <<-EOF > authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
+EOF
 chown -R $VAGRANT_USER:$VAGRANT_USER $VAGRANT_HOME/.ssh
 chmod 600 authorized_keys
-
+md5sum authorized_keys
 
 date > /etc/vagrant_box_build_time
