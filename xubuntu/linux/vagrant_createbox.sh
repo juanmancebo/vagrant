9fd03c087088126cb536036c3e711b0379e9f9b6
juan.mancebo <juanbaio10@gmail.com>
2020-08-02 22:38:57 +0200

    added i386 support

--- xubuntu/linux/vagrant_createbox.sh
+++ xubuntu/linux/vagrant_createbox.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-VM="xubuntu_2004"
-ISOFILE="xubuntu-20.04-desktop-amd64.iso"
-ISOMD5="c8977ce50d175dfce8e309dcaef8f1b3"
+VM="xubuntu"
+
+ISOFILE_32="xubuntu-18.04-desktop-i386.iso"
+ISOFILE_64="xubuntu-20.04-desktop-amd64.iso"
+ISOMD5_32="f74f01a3b7eeb99e8e28d8b9e2881cfc"
+ISOMD5_64="c8977ce50d175dfce8e309dcaef8f1b3"
+if egrep -qw "vmx|svm" /proc/cpuinfo
+	then 
+		echo "virtualization extension supported"
+		hwvirtex=on
+		ISOFILE=${ISOFILE_64}
+		ISOMD5=${ISOMD5_64}
+		RELEASE=20.04
+		OSTYPE=Ubuntu_64
+	else 
+		echo "no virtualization extension supported"
+		hwvirtex=off
+		pae=on
+		ISOFILE=${ISOFILE_32}
+		ISOMD5=${ISOMD5_32}
+		RELEASE=18.04
+		OSTYPE=Ubuntu
+fi
+
 if [ -f ${ISOFILE} ]
 then
 	echo "iso file already exists"
@@ -12,11 +33,11 @@ then
 	else
 		echo "md5 iso file does not match. Downloading new one..."
 		rm -f ${ISOFILE}
-		wget http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/20.04/release/$ISOFILE
+		wget http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/${RELEASE}/release/$ISOFILE
 	fi
 else
 	echo "Downloading iso file..."
-	wget http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/20.04/release/$ISOFILE
+	wget http://ftp.free.fr/mirrors/ftp.xubuntu.com/releases/${RELEASE}/release/$ISOFILE
 fi
 
 if vboxmanage list vms|grep -qw $VM
@@ -28,7 +49,7 @@ fi
 
 
 
-vboxmanage createvm --name $VM --ostype "Ubuntu_64" --register
+vboxmanage createvm --name $VM --ostype ${OSTYPE} --register
 vboxmanage storagectl $VM --name "IDE" --add ide
 vboxmanage storageattach $VM --storagectl "IDE" --port 1 --device 0 --type dvddrive --medium ${ISOFILE}
 vboxmanage createhd --filename ${VM}.vdi --size 20000
@@ -41,8 +62,9 @@ vboxmanage modifyvm $VM --pae off
 vboxmanage modifyvm $VM --graphicscontroller vmsvga
 vboxmanage modifyvm $VM --audioout on --audioin off
 vboxmanage modifyvm $VM --clipboard bidirectional --draganddrop bidirectional
+vboxmanage modifyvm $VM --hwvirtex ${hwvirtex} --pae ${pae}
 
-vboxmanage unattended install $VM --iso=${ISOFILE} --user=vagrant --full-user-name="vagrant" --password vagrant --hostname=vagrant.local --install-additions --locale=en_US --country=US --language=en-US --time-zone=UTC --script-template=/usr/lib/virtualbox/UnattendedTemplates/ubuntu_preseed.cfg
+vboxmanage unattended install $VM --iso=${ISOFILE} --user=vagrant --full-user-name="vagrant" --password vagrant --hostname=vagrant.local --install-additions --locale=en_US --country=US --language=en-US --time-zone=UTC --script-template=/usr/lib/virtualbox/UnattendedTemplates/ubuntu_preseed.cfg --post-install-template=/usr/lib/virtualbox/UnattendedTemplates/debian_postinstall.sh
 
 vboxmanage startvm $VM --type headless
 
